5707c724c14c6f1b0ae132ca59e44056ed3bd3e8
Davide Ponzini <davide.ponzini95@gmail.com>
2023-05-26 10:14:10 +0200

    removed my own signature

--- src/dav_tools/__init__.py
+++ src/dav_tools/__init__.py
@@ -4,7 +4,5 @@ from . import _arg_parser as _arg_parser
 from ._arg_parser import ArgumentAction
 
 argument_parser = _arg_parser.ArgumentParser()
-argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
-
 
 # ...
