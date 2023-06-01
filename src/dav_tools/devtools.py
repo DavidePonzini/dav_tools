7e98d4ddf8948fb4c850b4563766bcd92cb4ca76
Davide Ponzini <davide.ponzini95@gmail.com>
2023-06-01 16:32:16 +0200

    import from relative location

--- src/dav_tools/devtools.py
+++ src/dav_tools/devtools.py
@@ -1,6 +1,16 @@
 import importlib as _importlib
 from types import ModuleType as _ModuleType
+import sys as _sys
+import os as _os
 
 def reload_module(module: _ModuleType):
     _importlib.reload(module)
 
+def import_from(folder: str, module: str):
+    try:
+        path = _os.path.abspath(folder)
+        _sys.path.insert(1, path)
+
+        _importlib.import_module(module)
+    finally:
+        _sys.path.remove(path)
