c6e76713ff513655170cef6b1029e30718b1f318
Davide Ponzini <davide.ponzini95@gmail.com>
2024-05-30 12:13:54 +0200

    documentation

--- src/dav_tools/database.py
+++ src/dav_tools/database.py
@@ -1,7 +1,10 @@
+'''Database interaction'''
+
 import psycopg2 as _psycopg2
 from psycopg2 import sql as _sql
 
 class PostgreSQL:
+    '''Connection with a PostgreSQL database'''
     def __init__(self, database: str, host: str, user: str, password: str) -> None:        
         self._connection = _psycopg2.connect(database=database,
                                 host=host,
@@ -11,6 +14,23 @@ class PostgreSQL:
         self._cursor = self._connection.cursor()
 
     def insert(self, schema: str, table: str, data: dict[str, any], commit: bool = True, return_fields: list[str] = []):
+        '''
+        Inserts a row into a specified table within a PostgreSQL database.
+
+        :param schema: The schema name where the table resides.
+        :param table: The name of the table to insert data into.
+        :param data: A dictionary where keys are column names and values are the data to insert.
+        :param commit: Whether to commit the transaction after the insert. Defaults to True.
+        :param return_fields: A list of fields to return after the insert. Defaults to an empty list.
+
+        :returns: If `return_fields` is specified, returns a tuple containing the values of the requested fields. 
+                    Otherwise, returns None.
+
+        :notes: 
+            - The `data` dictionary keys must match the column names of the table.
+            - If `commit` is False, the changes must be committed manually using the connection's `commit` method.
+        '''
+        
         if len(return_fields) > 0:
             base_query = 'INSERT INTO {schema}.{table}({fields}) VALUES({values}) RETURNING {return_fields}'
         else:
