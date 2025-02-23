000dc5085776389e4a538df11e00a0c49e2b0f29
Davide Ponzini <davide.ponzini95@gmail.com>
2025-02-23 18:12:22 +0100

    more tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,16 @@
 from dav_tools import commands
 import platform
 
+
+def test_get_output_ok():
+    assert commands.get_output('echo TEST123', on_success=lambda x: x.decode(), stdin=None) == 'TEST123\n'
+
+def test_get_output_error():
+    if platform.system() == 'Windows':
+        assert commands.get_output('cmd /c exit 1', on_success=lambda x: x.decode(), on_error=lambda: b'ERROR', stdin=None) == 'ERROR'
+    else:
+        assert commands.get_output('false', on_success=lambda x: x.decode(), on_error=lambda: b'ERROR', stdin=None) == 'ERROR'
+
 def test_is_installed_existing():
     if platform == 'Windows':
         assert commands.is_installed('cmd')
