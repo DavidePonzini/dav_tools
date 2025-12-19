fad1f9fea3e5e3fda4833fec1156cdcbe0b96312
Davide Ponzini <davide.ponzini95@gmail.com>
2025-12-19 10:07:25 +0100

    moved main to separate file for requirements

--- src/dav_tools/requirements/__init__.py
+++ src/dav_tools/requirements/__init__.py
@@ -1,6 +1,6 @@
 '''Requirements for running a progam.'''
 
-from . import messages as _messages
+from .. import messages as _messages
 
 import os as _os
 import ctypes as _ctypes
@@ -43,20 +43,5 @@ def _require_os(*os: str) -> None:
         _messages.critical_error('OS not supported')
 
 
-if __name__ == '__main__':
-    '''Allow requirements from other programs'''
-    from . import argument_parser, ArgumentAction
-
-    argument_parser.set_description('Set script requirements')
-    argument_parser.set_developer_info('Davide Ponzini', 'davide.ponzini95@gmail.com')
-
-    argument_parser.add_argument('--root', action=ArgumentAction.STORE_TRUE, help='script needs to be run with admin privileges')
-    argument_parser.add_argument('--os', nargs='+', help='script can only be run on these OSes')
-
-    if argument_parser.args.root:
-        _require_root(auto_elevate=False)
-
-    if argument_parser.args.os:
-        _require_os(*argument_parser.args.os)
 
     
